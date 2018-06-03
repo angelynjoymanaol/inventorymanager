f60610b764f8b6baf9484292866279779e9c4c81
angelynjoymanaol <39758504+angelynjoymanaol@users.noreply.github.com>
2018-06-04 06:27:55 +0800

    Add files via upload

---
+++ reportforexchange2.php
@@ -0,0 +1,28 @@
+
+
+
+<?php
+
+
+$begin=$_REQUEST['begin'];
+$last=$_REQUEST['last'];
+setcookie('begin',$begin);
+setcookie('last',$last);
+
+include 'dbconnect.inc';
+$query="SELECT * FROM forexchange WHERE date >= '$_REQUEST[begin]'  AND date <= '$_REQUEST[last]'";
+$result=mysql_query($query);
+$num_results=mysql_num_rows($result);
+
+while($row=mysql_fetch_assoc($result))
+{
+if($_REQUEST['begin'] >= $row['date'])
+{
+
+
+header('Location:printreportforexchange.php');
+}}
+
+?>
+
+
