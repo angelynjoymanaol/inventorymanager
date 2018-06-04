7dbca91f360b4beff64e30cd0d97f0514a84225f
Unknown <angelynjoy.manalo@gmail.com>
2018-06-04 16:40:36 +0800

    log

--- login.php
+++ login.php
@@ -176,9 +176,11 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav">
-     
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <li class="nav-item active">
-        <a class="nav-link" href="home.php">Home <span></span><span></span></a>
+        <a class="nav-link" href="home.php">Login <span></span><span></span></a>
       </li>
       
      
